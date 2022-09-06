9b4ea1690f9317d07808c14434455995e7b30323
Ilia Deriglazov <m313am@gmail.com>
2022-09-06 13:10:56 -0400

    Added logfile in UseCaseBase

--- src/main/java/Utilities/UseCaseBase.java
+++ src/main/java/Utilities/UseCaseBase.java
@@ -3,9 +3,16 @@ import Pages.BasePage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.logging.Level;
 
 public class UseCaseBase {
     protected static WebDriver webDriver;
@@ -16,12 +23,13 @@ public class UseCaseBase {
         page = new BasePage();
         webDriver = SharedDriver.getWebDriver(SharedDriver.Broswer.CHROME);
         page.setWebDriver(webDriver);
+        System.setProperty("webdriver.chrome.logfile", "C:\\chromedriver.log");
+        System.setProperty("webdriver.chrome.verboseLogging", "true");
     }
-/*
+
     @AfterAll
     public static void tearDownMain() {
         SharedDriver.closeDriver();
         webDriver = null;
     }
-*/
 }
